93dc9a0af1ad3c722ee4ff23fdcf2e45505b4af4
Mitch Emerson <mitchemerson@users.noreply.github.com>
2016-05-03 19:19:07 -0500

    fixed Add Director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,7 +8,7 @@ class DirectorsController < ApplicationController
   end
 
   def new_form
-
+    
   end
 
   def create_row
@@ -30,10 +30,10 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
