2f3997a91048131d87d3da242e2aec164a408b15
alathans <alathans@users.noreply.github.com>
2016-05-05 17:33:21 -0500

    gah!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,23 +31,25 @@ class MoviesController < ApplicationController
   end
 
   def edit_form
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find_by({:id => params[:id]})
   end
 
   def update_row
+    @movie = Movie.find_by({:id => params[:id]})
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
-
+    @movie.save
     render("show")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
+    @movie = Movie.find_by({:id => params[:id]})
+    @movie.destroy
 
-    movie.destroy
+    render("index")
   end
 end
