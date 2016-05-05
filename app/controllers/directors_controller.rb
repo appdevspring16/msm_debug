0a3d508cd1e14544b2411fa81312b2b6688ff26e
Maayan <maharon@>
2016-05-04 21:08:55 -0500

    MA_msm_debug_final

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,13 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
-    @director = Director.find(params[:id])
+    @director = Director.find_by( { :id=> params[:id]} )
   end
 
-  def new
+  def new_form
   end
 
   def create_row
@@ -16,10 +16,9 @@ class DirectorsController < ApplicationController
     @director.name = params[:name]
     @director.bio = params[:bio]
     @director.image_url = params[:image_url]
-
     @director.save
 
-    render("show")
+    redirect_to("http://localhost:3000/directors")
   end
 
   def edit_form
@@ -28,16 +27,14 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
-
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
-
+    @director.dob = params[:the_dob]
+    @director.name = params[:the_name]
+    @director.bio = params[:the_bio]
+    @director.image_url = params[:the_image_url]
     @director.save
 
-    render("show")
-  end
+render("show")
+ end
 
   def destroy
     @director = Director.find(params[:id])
