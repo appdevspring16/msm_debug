cffa8f52f9c467a31cd9df4d10a7749b2d24d65f
Josh Charney <jfcharney@user.noreply.github.com>
2016-05-07 11:57:26 -0500

    Ready for feedback
    done.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,16 +1,18 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params[:id])
+
+    render("show_details")
   end
 
-  def new
+  def new_form
   end
 
-  def create_row
+  def create_form
     @director = Director.new
     @director.dob = params[:dob]
     @director.name = params[:name]
@@ -28,15 +30,26 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
+    @director.dob = params[:the_dob]
+    @director.name = params[:the_name]
+    @director.bio = params[:the_bio]
+    @director.image_url = params[:image_url]
+    @director.save
+    render("show_details")
+  end
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+
+  def create_row
+    # @director = Director.find(params[:id])
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    render("show_details")
   end
 
   def destroy
