dbc72ac48bd2d8678d913276e5d0235c01b66d96
Andrea Ramirez <aramirez@users.noreply.github.com>
2016-05-13 12:18:37 -0500

    up to view and edit movies

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,11 @@ Rails.application.routes.draw do
   get('/movies/new_form', { :controller => 'movies', :action => 'new_form' })
   get('/create_movie', { :controller => 'movies', :action => 'create_row' })
 
+  # UPDATE
+  get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' })
+  get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
+
+
   # READ
   get('/movies', { :controller => 'movies', :action => 'index' })
   get('/movies/:id', { :controller => 'movies', :action => 'show' })
