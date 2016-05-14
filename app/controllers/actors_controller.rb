db0154c4bd672d5ad71fd561c895fa296d74d6c2
Emily Gikow <egikow@chicagobooth.edu>
2016-05-14 10:34:02 -0500

    movies-fixed

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -28,12 +28,10 @@ class ActorsController < ApplicationController
 
   def update_row
     @actor = Actor.find(params[:id])
-
     @actor.dob = params[:dob]
     @actor.name = params[:name]
     @actor.bio = params[:bio]
     @actor.image_url = params[:image_url]
-
     @actor.save
 
     render("show")
